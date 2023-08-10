f0d75d669f32cc985b8b9c577281ef126c71b24b
varshini <sivavarshini69@gamil.com>
2023-08-10 12:50:20 +0530

    first

---
+++ server.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express()
+
+//routes
+app.get('/',(req,res)=>{
+    res.send("Hello member")
+})
+app.listen(3000,()=>{
+    console.log(`Node is running on 3000`)
+})
