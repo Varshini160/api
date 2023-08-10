d1953d45c7bd9aafef7e2945ff4017f6e4e46dab
varshini <sivavarshini69@gamil.com>
2023-08-10 20:43:55 +0530

    final

--- server.js
+++ server.js
@@ -1,10 +1,83 @@
 const express=require('express')
+const mongoose=require('mongoose')
+const Product=require('./models/productModels')
 const app=express()
 
+app.use(express.json())
 //routes
 app.get('/',(req,res)=>{
     res.send("Hello member")
 })
-app.listen(3000,()=>{
-    console.log(`Node is running on 3000`)
+app.get('/blog',(req,res)=>{
+    res.send("Hello super Varshini")
+})
+app.get('/products',async(req,res)=>{
+    try{
+        const products=await Product.find({});
+        res.status(200).json(products);
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+
+app.get('/products/:id',async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const product=await Product.findById(id);
+        res.status(200).json(product);
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+
+app.post('/products',async(req,res)=>
+{
+    try{
+        const product=await Product.create(req.body)
+        res.status(200).json(product);
+    }
+    catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.put('/products/:id',async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const product=await Product.findByIdAndUpdate(id,req.body)
+        if(!product){
+            return res.status(404).json({message:`cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
+    }
+    catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+app.delete('/products/:id',async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const product=await Product.findByIdAndDelete(id)
+        if(!product){
+            return res.status(404).json({message:`cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
+    }
+    catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+mongoose.connect('mongodb+srv://admin:739GRmGRBANPztue@cluster0.ea5u0km.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>{
+    console.log("connected")
+    app.listen(3000,()=>{
+        console.log(`Node is running on 3000`)
+    });
+}).catch((error)=>{
+    console.log(error)
 })
